876ee7a97aef7fb91c3d2f9fc09a183cb3d556f2
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-03-19 22:26:07 -0300

    finish POST endpoint and add tests

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,9 +3,11 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -22,3 +24,12 @@ def get_html():
         </head>
     </html>
     """
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,32 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_return_ok_hello_world():
-    client = TestClient(app)
-
+def test_root_return_ok_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_should_return_html():
-    client = TestClient(app)
-
+def test_should_return_html(client):
     response = client.get('/html')
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1>Olá Mundo</h1>' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
